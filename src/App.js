3ce09ec1bce621711c6379acfd36e98834e4fe28
gabei <gabe1792@gmail.com>
2023-12-09 14:45:56 -0600

    added some styling to get the editor and markdown boxes in tehri appropriate spots

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Editor from './Editor/Editor';
 import Preview from './Preview/Preview';
 import { marked } from 'marked';
