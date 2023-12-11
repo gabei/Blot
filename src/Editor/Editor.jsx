f187d0ef779148d344ed0804d2f7a452b20b4432
gabei <gabe1792@gmail.com>
2023-12-11 12:43:01 -0600

    figured out how to reference child style for height adjustment using react refs

--- src/Editor/Editor.jsx
+++ src/Editor/Editor.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './Editor.scss';
 
 class Editor extends Component {
@@ -6,16 +6,30 @@ class Editor extends Component {
         super(props);
 
         this.handleChange = this.handleChange.bind(this);
+        this.heightRef = React.createRef();
     }
 
     handleChange(e){
         this.props.update(e.target.value);
+        this.updateHeight();
+    }
+
+    updateHeight(){
+        console.log(this.heightRef.current.scrollHeight);
+        /*
+        Update the Editor and Text Area's height to match the textarea's scrollHeight. This prevents a scroll bar from appearing. Once these elements reach set max-height, stop adjusting.
+        */
     }
 
     render(){
+
+
+
         return (
         <div className="Editor">
-            <textarea onChange={this.handleChange}></textarea>
+            <textarea 
+                ref={this.heightRef}
+                onChange={this.handleChange}></textarea>
         </div>
         )
     }
