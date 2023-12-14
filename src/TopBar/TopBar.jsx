f46306169c9dda6e1544784865e2c78b6ac809d3
Admin <admin@LTLIBRARY.ORG>
2023-12-14 17:43:58 -0600

    added animations to logo. looks neat

--- src/TopBar/TopBar.jsx
+++ src/TopBar/TopBar.jsx
@@ -4,7 +4,12 @@ import './TopBar.scss';
 export default function TopBar(props){
     return (
         <div className="TopBar">
-            <div className="logo"></div>
+            <div className="logo">
+                <div className="logo--shaper-1"></div>
+                <div className="logo--shaper-2"></div>
+                <div className="logo--shaper-3"></div>
+                <div className="logo--shaper-4"></div>
+            </div>
             <div className="header-box">
                 <h1>Blotter</h1>
                 <h2>Markdown Previewer</h2>
